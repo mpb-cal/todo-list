020fab50cdef48e9050b3fe4f31a86c4b7f35f48
Marc Bernasconi <steelstring@protonmail.com>
2020-01-04 21:20:54 -0800

    Updates

---
+++ precache-manifest.e0383bcd64b2deecf053e1894375a54b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea37d3b9bc7eaf7ec15939e698253c38",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "42935e7b3efd40e1a90d",
+    "url": "/todo-list/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "8400078c793d6ce03f0d",
+    "url": "/todo-list/static/css/main.214812c9.chunk.css"
+  },
+  {
+    "revision": "42935e7b3efd40e1a90d",
+    "url": "/todo-list/static/js/2.06019cf4.chunk.js"
+  },
+  {
+    "revision": "8400078c793d6ce03f0d",
+    "url": "/todo-list/static/js/main.ff7e0d8f.chunk.js"
+  },
+  {
+    "revision": "2707f47aff01670315bc",
+    "url": "/todo-list/static/js/runtime-main.004b27d9.js"
+  }
+]);
