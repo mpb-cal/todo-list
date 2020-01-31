bfde2ddad3df4899cfbb3a5443600ab374aea690
Marc Bernasconi <marc.p.bernasconi@macys.com>
2020-01-31 15:57:00 -0800

    added import button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 //import { C, items } from './actions';
 import { items } from './actions';
-import sampleState from './sampleState.json';
+import sampleState from './sampleStateMPB.json';
 import ActionItemManagerRedux from './action-item-manager-redux';
 
 //const STORAGE_KEY = 'action-item-manager-state';
@@ -21,7 +21,8 @@ function App() {
 
   const store = createStore(
     combineReducers({items}),
-    JSON.parse(localStorage.getItem(STORAGE_KEY_REDUX)) || sampleState
+//    JSON.parse(localStorage.getItem(STORAGE_KEY_REDUX)) || 
+      sampleState
   );
 
   store.subscribe(() => {
